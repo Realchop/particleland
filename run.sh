0377f5c9167e193273d87849edf3b2e6e7cdc4d5
realchop <jovanovic.lazar002@gmail.com>
2024-10-03 19:46:39 +0200

    Update build command

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ fi
 
 mkdir -p builds &&
 cd src &&
-gcc -g3 main.c particle.c shared_memory_boiler_plate.c ../protocols/xdg-shell.c ../protocols/wlr_shell.c -l wayland-client -L /usr/include/cairo -l cairo -o ../builds/$1 &&
+gcc -g3 main.c particle.c shared_memory_boiler_plate.c ../protocols/xdg-shell.c ../protocols/wlr_shell.c -l wayland-client -l SDL2 -l SDL2_gfx -o ../builds/$1 &&
 cd .. &&
 ./builds/$1
